e2f8927adf7642d59b60913dc7500d363b07b937
aota18 <sapphire031794@gmail.com>
2021-07-06 15:15:15 +0900

    server address update

--- src/lib/api/weather.ts
+++ src/lib/api/weather.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const baseUrl='http://localhost:3000';
+const baseUrl='http://3.35.22.199:3000';
 
 export const getWeatherInfo = ({lat, lgt}) => axios.get(`${baseUrl}/weather?lat=${lat}&lgt=${lgt}`);
